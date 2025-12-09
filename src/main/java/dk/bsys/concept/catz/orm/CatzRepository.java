ad2fa16c75c4ea8c77ff46bedf67cadc4d77c040
Brian Westy <brian.w.rasmussen@ufst.dk>
2025-12-09 08:33:56 +0100

    First Catz JPA test

--- src/main/java/dk/bsys/concept/catz/orm/CatzRepository.java
+++ src/main/java/dk/bsys/concept/catz/orm/CatzRepository.java
@@ -3,4 +3,6 @@ package dk.bsys.concept.catz.orm;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 interface CatzRepository extends JpaRepository<Catz, Long> {
+
+    Catz findByName(String name);
 }
